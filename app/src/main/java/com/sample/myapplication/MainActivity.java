1915828b1ec67ca7d586fd19b9be955dc309f945
victory.turpin <vturp@yahoo.com>
2016-04-21 20:46:53 -0400

    Initial commit

---
+++ app/src/main/java/com/sample/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sample.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import java.util.function.*;
+import java.util.Comparator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    interface Person{
+
+        int getAge();
+    }
+
+    public void init(){
+
+        System.out.println("Hello Lambda");
+
+
+
+        Comparator<String> comparator2 = (String s1, String s2) -> Integer.compare(s2.length(), s1.length());
+        Runnable r = () -> {
+            int i = 0;
+            while(i++ < 10){
+                System.out.println("It works");
+            }
+        };	// end Runnable
+
+        Person p = () -> {
+            return 8;
+        };
+
+//        Consumer<String> printer = s -> System.out.println(s);
+    }
+}
